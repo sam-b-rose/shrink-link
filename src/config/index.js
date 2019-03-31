adcf47ebc7c5520e87ac35a40f085b39ac406ec2
Sam Rose <samrose3@gmail.com>
2019-03-31 14:31:50 -0500

    refactor(uid): remove auto id and add better uid for URL model
    Closes #5

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,6 @@
-const { merge } = require('lodash')
+const dotenv = require('dotenv')
+dotenv.config()
+
 const env = process.env.NODE_ENV || 'development'
 const dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb://localhost:27017/shrink-link'
 
